14b2bcb5cf5e3fc7f3da893d104f9e7cec32cc42
Vipul Singh <104285752+Vipul123kumar@users.noreply.github.com>
2025-04-10 18:52:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 console.log("STD");
 
@@ -11,11 +11,11 @@ console.log("STD");
 root.render(
  
   
-  <BrowserRouter>
+  <HashRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-   </BrowserRouter>
+   </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
